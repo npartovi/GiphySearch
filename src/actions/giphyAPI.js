eb4aed344fa58c32a71c56d12b0faf1d0968984d
npartovi <npartovi@gmail.com>
2018-10-12 12:51:17 -0700

    api routes for trending and search done

--- src/actions/giphyAPI.js
+++ src/actions/giphyAPI.js
@@ -1,5 +1,25 @@
-import keys from './actions/giphyAPI'
+import axios from 'axios'
+import keys from '../config'
 
 export const giphyTrending = () => {
 
+    axios.get(`http://api.giphy.com/v1/gifs/trending?limit=25&api_key=${keys.giphyAPIKey}`)
+        .then(res => {
+            console.log("trending",res.data)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+    
+}
+
+export const giphySearch = (term) => {
+
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${term}&limit=25&api_key=${keys.giphyAPIKey}`)
+        .then(res => {
+            console.log("search",res.data)
+        })
+        .catch(err => {
+            console.error(err)
+        })
 }
