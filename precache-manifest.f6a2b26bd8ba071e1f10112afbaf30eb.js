8360d51aa1ccce018911ed6e361e8e46dac54839
npartovi <npartovi@gmail.com>
2018-10-15 01:39:01 -0700

    Updates

---
+++ precache-manifest.f6a2b26bd8ba071e1f10112afbaf30eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd7d23cd39e3f6f019f1",
+    "url": "/eaze_challenge/static/js/runtime~main.cd7d23cd.js"
+  },
+  {
+    "revision": "4846b5810ceea5f3cb7d",
+    "url": "/eaze_challenge/static/js/main.4846b581.chunk.js"
+  },
+  {
+    "revision": "c72577bc4e276e429deb",
+    "url": "/eaze_challenge/static/js/1.c72577bc.chunk.js"
+  },
+  {
+    "revision": "4846b5810ceea5f3cb7d",
+    "url": "/eaze_challenge/static/css/main.3ca9284b.chunk.css"
+  },
+  {
+    "revision": "b80d65b45d63fb603e765acda0a4b6a5",
+    "url": "/eaze_challenge/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/eaze_challenge/precache-manifest.f6a2b26bd8ba071e1f10112afbaf30eb.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/eaze_challenge/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
